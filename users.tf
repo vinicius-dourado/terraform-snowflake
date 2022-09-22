4270d0d2fcba63cd3ef67342c68d5ce2762a17c4
Vinícius Dourado <viuaraujo@gmail.com>
2022-09-22 15:29:38 -0300

    Update users.tf

--- users.tf
+++ users.tf
@@ -2,3 +2,19 @@ resource "snowflake_user" "user_dbt" {
   name    = "dbt"
   comment = "for testing"
 }
+
+resource "snowflake_user" "user_etl" {
+  name    = "etl"
+  comment = "for testing"
+}
+
+resource "snowflake_user" "user_gabriel" {
+  name    = "gabriel"
+  comment = "for testing"
+  default_role = "data_engineer_role"
+}
+
+resource "snowflake_user" "user_joao" {
+  name    = "joao"
+  comment = "for testing"
+}
