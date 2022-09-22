3c25ef0d46b0181f4664e6bebb2435b99725d36a
Vinícius Dourado <viuaraujo@gmail.com>
2022-09-22 16:04:59 -0300

    Update access_roles.tf

--- access_roles.tf
+++ access_roles.tf
@@ -1,19 +1,19 @@
-resource "snowflake_role" "raw_full" {
+resource "snowflake_role" "raw_full_role" {
   name    = "raw_full"
   comment = "for testing"
 }
 
-resource "snowflake_role" "integration_full" {
+resource "snowflake_role" "integration_full_role" {
   name    = "integration_full"
   comment = "for testing"
 }
 
-resource "snowflake_role" "integration_r" {
+resource "snowflake_role" "integration_r_role" {
   name    = "integration_r"
   comment = "for testing"
 }
 
-resource "snowflake_role" "datamart_rw" {
+resource "snowflake_role" "datamart_rw_role" {
   name    = "datamart_rw"
   comment = "for testing"
 }
