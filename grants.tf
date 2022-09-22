6a4f3261a405a45987b26fdaca9b29eac8118265
Vinícius Dourado <viuaraujo@gmail.com>
2022-09-22 16:26:26 -0300

    Update grants.tf

--- grants.tf
+++ grants.tf
@@ -6,12 +6,3 @@ resource "snowflake_database_grant" "integration_r_grant" {
 
   with_grant_option = false
 }
-
-resource "snowflake_database_grant" "datamart_rw_grant" {
-  database_name = "integration_db"
-
-  privilege = ["SELECT","INSERT","DELETE","UPDATE"]
-  roles     = ["integration_r"]
-
-  with_grant_option = false
-}
