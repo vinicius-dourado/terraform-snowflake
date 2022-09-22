7cfee9647ae3b5fe290ed0fc33cbaf46c79e67d8
Vinícius Dourado <viuaraujo@gmail.com>
2022-09-22 15:00:03 -0300

    Create access_roles.tf

---
+++ access_roles.tf
@@ -0,0 +1,19 @@
+resource "snowflake_role" "raw_full" {
+  name    = "raw_full"
+  comment = "for testing"
+}
+
+resource "snowflake_role" "integration_full" {
+  name    = "integration_full"
+  comment = "for testing"
+}
+
+resource "snowflake_role" "integration_r" {
+  name    = "integration_r"
+  comment = "for testing"
+}
+
+resource "snowflake_role" "datamart_rw" {
+  name    = "datamart_rw"
+  comment = "for testing"
+}
