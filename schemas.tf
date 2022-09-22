ebfe76f2eb033c8d44a6eb52a835ae9c64f2231a
Vinícius Dourado <viuaraujo@gmail.com>
2022-09-22 12:41:48 -0300

    Update schemas.tf

--- schemas.tf
+++ schemas.tf
@@ -3,3 +3,9 @@ resource "snowflake_schema" "demo_schema" {
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
+
+resource "snowflake_schema" "demo_schema_dev" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA_DEV"
+  comment  = "Schema for Snowflake Terraform demo"
+}
