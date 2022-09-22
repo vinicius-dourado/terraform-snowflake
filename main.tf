e1d3a95fdd815831c2a50744f46fe82a5930c4c9
Vinícius Dourado <viuaraujo@gmail.com>
2022-09-22 11:48:53 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "vtad"
+
+    workspaces {
+      name = "gh-actions-snowflake"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
