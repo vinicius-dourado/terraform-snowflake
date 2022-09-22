2172f1e1c015479898d79a360ab09d368b2b8708
Vinícius Dourado <viuaraujo@gmail.com>
2022-09-22 15:58:05 -0300

    Update roles_assignments.tf

--- roles_assignments.tf
+++ roles_assignments.tf
@@ -11,6 +11,19 @@ resource "snowflake_role_grants" "etl_role_assign" {
 
 }
 
+resource "snowflake_role_grants" "data_engineer_assign" {
+  role_name = "data_engineer_role"
+
+  roles = [
+    "ACCOUNTADMIN",
+  ]
+
+  users = [
+    "gabriel",
+  ]
+
+}
+
 resource "snowflake_role_grants" "data_scientist_assign" {
   role_name = "data_scientist_role"
 
@@ -37,13 +50,5 @@ resource "snowflake_role_grants" "dbt_role_assign" {
 
 }
 
-resource "snowflake_role_grants" "raw_full_assign" {
-  role_name = "raw_full"
 
-  roles = [
-    "etl_role",
-    "data_engineer_role",    
-  ]
-
-}
 
