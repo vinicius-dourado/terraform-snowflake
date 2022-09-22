a00a50b49d58784e651b443420202d632022127f
Vinícius Dourado <viuaraujo@gmail.com>
2022-09-22 15:48:44 -0300

    Update roles_assignments.tf

--- roles_assignments.tf
+++ roles_assignments.tf
@@ -10,3 +10,29 @@ resource "snowflake_role_grants" "etl_role_assign" {
   ]
 
 }
+
+resource "snowflake_role_grants" "data_scientist_assign" {
+  role_name = "data_scientist_role"
+
+  roles = [
+    "ACCOUNTADMIN",
+  ]
+
+  users = [
+    "joao",
+  ]
+
+}
+
+resource "snowflake_role_grants" "dbt_role_assign" {
+  role_name = "dbt_role"
+
+  roles = [
+    "ACCOUNTADMIN",
+  ]
+
+  users = [
+    "dbt",
+  ]
+
+}
