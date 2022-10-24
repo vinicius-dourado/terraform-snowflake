e6101c4b14399f94c21fcef210d235e881997bb8
Vinícius Dourado <viuaraujo@gmail.com>
2022-10-24 18:50:52 -0300

    Delete users.tf

--- users.tf
+++
@@ -1,9 +0,0 @@
-resource "snowflake_user" "user_dbt" {
-  name    = "dbt_user"
-  comment = "for testing"
-}
-
-resource "snowflake_user" "user_fivetran" {
-  name    = "fivetran_user"
-  comment = "for testing"
-}
